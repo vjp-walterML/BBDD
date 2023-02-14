2c05ee196a5e6e925ad5cc92be11751967173dee
vjp-walterML <114687981+vjp-walterML@users.noreply.github.com>
2023-02-14 19:56:22 +0100

    farmacia

---
+++ FARMACIA.sql
@@ -0,0 +1,242 @@
+-- BASE DE DATOS FARMACIA
+-- 1º DAW
+-- 14-02-2023
+-- WALTER MARTIN LOPES
+-- -------------------------------------------------------
+CREATE DATABASE FARMACIA;
+USE FARMACIA;
+
+-- ===========================CREACION DE TABLAS=======================================
+CREATE TABLE CLIENTES(
+ID_CLIENTE INT PRIMARY KEY,
+NOMBRE VARCHAR(50),
+APE1 VARCHAR(50),
+APE2 VARCHAR(50),
+DNI VARCHAR(9),
+DIRECCION VARCHAR(50),
+TELEFONO CHAR(9));
+
+CREATE TABLE EMPLEADOS(
+ID_EMPLEADO INT,
+NOMBRE VARCHAR(50) NOT NULL,
+APE1 VARCHAR(50) NOT NULL,
+APE2 VARCHAR(50) NOT NULL,
+DNI VARCHAR(9) NOT NULL,
+DIRECCION VARCHAR(40),
+TELEFONO_EM CHAR(9),
+SUELDO DECIMAL(10,2) NOT NULL);
+
+CREATE TABLE MEDICAMENTOS(
+ID_MEDICAMENTO INT PRIMARY KEY,
+NOMCOMERCIAL VARCHAR(50) NOT NULL,
+PRINCIPIO DECIMAL(5,2) NOT NULL,
+LABORATORIO VARCHAR(50) NOT NULL);
+
+CREATE TABLE VENTAS(
+ID_VENTAS INT PRIMARY KEY,
+FECHA_VENTAS DATE NOT NULL,
+ID_CLIENTE INT,
+ID_EMPLEADO INT,
+ID_MEDICAMENTO INT);
+
+-- ==========================================PRIMARY KEY Y FOREIGN KEY===================================
+-- AÑADO LA PK DE LA TABLA EMPLEADOS Y LAS FK CORRESPONDIENTES DE LA TABLA VENTAS
+ALTER TABLE EMPLEADOS
+MODIFY ID_EMPLEADO INT PRIMARY KEY;
+
+DESC EMPLEADOS;
+
+-- FK
+ALTER TABLE VENTAS
+ADD CONSTRAINT ID_C_FK FOREIGN KEY(ID_CLIENTE) REFERENCES CLIENTES(ID_CLIENTE) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT ID_E_FK FOREIGN KEY(ID_EMPLEADO) REFERENCES EMPLEADOS(ID_EMPLEADO) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT ID_M_FK FOREIGN KEY(ID_MEDICAMENTO) REFERENCES MEDICAMENTOS(ID_MEDICAMENTO) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- ==============================INSERTAR DATOS=============================================
+-- MODIFICO CAMPO SALARIO PARA QUE TENGA 6 NUMEROS Y 2 DECIMALES
+ALTER TABLE EMPLEADOS
+MODIFY SUELDO DECIMAL(6,2) NOT NULL;
+
+-- AÑADO COLUMNA QUE SE ME HABIA OLVIDADO EN MEDICAMENTOS
+ALTER TABLE MEDICAMENTOS
+ADD COLUMN PRECIO DECIMAL(3,2) NOT NULL AFTER PRINCIPIO;
+DESC MEDICAMENTOS;
+
+ALTER TABLE MEDICAMENTOS 
+MODIFY COLUMN PRECIO DECIMAL(7,2) NOT NULL;
+
+ALTER TABLE MEDICAMENTOS
+MODIFY COLUMN PRINCIPIO VARCHAR(50) NOT NULL;
+
+-- INSERTO
+insert into clientes values
+(1,'JOSE','PEREZ','LOPEZ','11222222S','C/SOL','608112223'),
+(2,'ANTONIO','LOPEZ','MATEOS','12000333A','C/LUNA','912322323'),
+(3,'MARIA','SANCHEZ','SANCHEZ','09888777D','ARAPILES','665656678'),
+(4,'JESÚS','RAMOS','RAMOS','11876656F','LA MOTA','454545454'),
+(5,'CARMEN','DIAZ','SANCHEZ','06767887D','EL MONTE','927898989'),
+(6,'JUAN','PEREZ','PEREZ','23332233E','LA POZA','924565655'),
+(7,'LUISA','SANJUAN','LUNA','12898989J','GRANDE','924555555');
+
+
+INSERT INTO MEDICAMENTOS VALUES
+(1,'GELOCATIL','PARACETAMOL','2.20','FYB'),
+(2,'ALGIDOL','PARACETAMOL','2.25','BERENGUER'),
+(3,'MOTOSOL','AMBROXOL','3.46','EUROPHARM'),
+(4,'PHARMATON','VITAMINAS','6.20','EUROPHARM'),
+(5,'STREPSILS','ALCOHOL DICLOROBENCILO','3.56','BOOTS HEAL'),
+(6,'RHODOGIL','ESPIRAMICINA','7.89','RHONE POUL'),
+(7,'ASPIRINA','ACIDO ACETILSALICILICO','2.45','BAYER'),
+(8,'ALERGICAL','FLUOCINOLO','2.76', 'IQUINOSA'),
+(9,'PECTOX','CARBOCISTEINA','4.05','ITALFARMAC'),
+(10,'ANGILEPTOL','BENZOCAINA','4.45','AZQUE');
+
+INSERT INTO EMPLEADOS VALUES
+(1,'JUAN','RODRIGUEZ','RODRIGUEZ','12233445T','C/POLEN','609889977',1022.24),
+(2,'MARIA','GARCIA','GARCIA','23449987U','CLAVELLINAS','789900000',1044.79),
+(3,'JOSE','MARTINEZ','MARTINEZ','12776688P','LA ROSA','924677676',1552.13);
+
+INSERT INTO VENTAS
+VALUES 
+(1,'2009/1/21',2,2,1),
+(2,'2009/1/4',3,3,9),
+(3,'2010/1/30',4,1,8),
+(4,'2010/1/22',2,2,10),
+(5,'2010/2/12',2,3,8),
+(6,'2010/2/12',6,2,10),
+(7,'2011/2/12',2,1,5),
+(8,'2011/2/11',6,2,10),
+(9,'2013/2/14',2,1,4),
+(10,'2013/2/28',5,2,7),
+(11,'2013/3/1',7,3,2),
+(12,'2013/3/3',3,3,3),
+(13,'2013/3/3',2,2,3),
+(14,'2013/3/5',3,1,10),
+(15,'2013/3/04',5,2,4),
+(16,'2013/3/6',5,2,5),
+(17,'2013/3/12',4,3,5),
+(18,'2013/3/16',3,2,6),
+(19,'2013/3/2',4,1,10),
+(20,'2013/3/25',5,2,10);
+
+-- ========================2-MODIFICACION DE TABLAS Y DE DATOS==============================
+-- 1A 
+ALTER TABLE CLIENTES 
+MODIFY NOMBRE VARCHAR(50) NOT NULL,
+MODIFY APE1 VARCHAR(50) NOT NULL,
+MODIFY APE2 VARCHAR(50) NOT NULL;
+
+-- 1B
+CREATE INDEX I_CLIENTE_DNI ON CLIENTES(DNI);
+
+-- 2A
+ALTER TABLE EMPLEADOS 
+MODIFY NOMBRE VARCHAR(50);
+
+ALTER TABLE EMPLEADOS 
+MODIFY SUELDO DECIMAL(10,2) NOT NULL CHECK(SUELDO BETWEEN 1000 AND 2500);
+
+ALTER TABLE EMPLEADOS
+MODIFY DIRECCION VARCHAR(50);
+
+-- 3A
+UPDATE CLIENTES
+SET DIRECCION = 'Calle Mayor 5'
+WHERE APE1='Pérez';
+
+-- 3B
+UPDATE CLIENTES
+SET TELEFONO = '722748406'
+WHERE DNI= '’00000001A';
+
+-- 3C
+UPDATE EMPLEADOS
+SET SUELDO = SUELDO*1.1;
+
+-- 3D
+DELETE FROM CLIENTES 
+WHERE NOMBRE='Antonio López Mateos';
+
+-- 3E
+UPDATE MEDICAMENTOS
+SET PRECIO=5.25
+WHERE NOMCOMERCIAL='GELOCATIL';
+
+-- 3F
+UPDATE MEDICAMENTOS 
+SET LABORATORIO='BAYER'
+WHERE ID_MEDICAMENTO=7;
+
+-- 3G
+UPDATE MEDICAMENTOS
+SET PRECIO = PRECIO*1.05
+WHERE PRINCIPIO='Paracetamol';
+
+-- 3H
+DELETE FROM MEDICAMENTOS
+WHERE LABORATORIO = 'AZQUE';
+
+-- ========================3-CONSULTAS==============================
+-- 1. Crea una consulta que seleccione todos los registros de la tabla CLIENTES.
+SELECT * FROM CLIENTES;
+-- 2. Crea una consulta que seleccione todos los registros de la tabla EMPLEADOS.
+SELECT * FROM EMPLEADOS;
+-- 3. Crea una consulta que seleccione todos los registros de la tabla MEDICAMENTOS.
+SELECT * FROM MEDICAMENTOS;
+-- 4. Crea una consulta que muestre el nombre, ape1, ape2 y teléfono de los clientes.
+SELECT NOMBRE, APE1, APE2 , TELEFONO FROM CLIENTES;
+-- 5. Crea una consulta que muestre el ID_cliente y la dirección de los clientes.
+SELECT ID_CLIENTE,DIRECCION FROM CLIENTES;
+-- 6. Crea una consulta que muestre el nombre, ape1, ape2 y sueldo de los empleados.
+SELECT NOMBRE,APE1,APE2, SUELDO FROM EMPLEADOS;
+-- 7. Haz la misma consulta del ejercicio 6, pero ahora el campo ape1 se mostrará con la etiqueta
+-- PRIMER APELLIDO, el campo ape2 con SEGUNDO APELLIDO, y para el campo sueldo aparecerá en
+-- la cabecera SALARIO.
+SELECT NOMBRE,APE1 AS PRIMER_APELLIDO,APE2 AS SEGUNDO_APELLIDO, SUELDO AS SALARIO FROM EMPLEADOS;
+-- 8. Crea una consulta que muestre los laboratorios que fabrican los medicamentos, sin duplicados.
+SELECT DISTINCT LABORATORIO  FROM MEDICAMENTOS;
+-- 9. Crea una consulta que muestre el nombre, ape1, ape2 y teléfono del cliente cuyo Id_cliente=2
+SELECT NOMBRE, APE1,APE2,TELEFONO FROM CLIENTES
+WHERE ID_CLIENTE=2;
+-- 10. Crea una consulta que muestre el ID_cliente y la dirección de los clientes cuyo segundo apellido es SANCHEZ O SÁNCHEZ.
+SELECT ID_CLIENTE,DIRECCION FROM CLIENTES
+WHERE APE2 IN('SANCHEZ','SÁNCHEZ');
+-- 11. Crea una consulta que muestre el nombre, ape1, ape2 y sueldo de los empleados con ID_empleado 2 y 3.
+SELECT NOMBRE,APE1,APE2,SUELDO FROM EMPLEADOS
+WHERE ID_EMPLEADO IN(2,3);
+-- 12. Crea una consulta que muestre el nombre, ape1, ape2 de los empleados con sueldo mayor de 2000.
+SELECT NOMBRE,APE1,APE2 FROM EMPLEADOS
+WHERE SUELDO>2000;
+-- 13. Crea una consulta que muestre el nombre y dirección de los empleados cuyo sueldo es distinto de 1500.
+SELECT NOMBRE,DIRECCION FROM EMPLEADOS 
+WHERE SUELDO != 1500;
+-- 14. Crea una consulta que muestre el nombre, ape1 de los empleados con sueldo comprendido entre 1000 y 3000 (SIN LOS VALORES EXTREMOS).
+SELECT NOMBRE,APE1,SUELDO FROM EMPLEADOS
+WHERE SUELDO > 1000 AND SUELDO < 3000;
+-- 15. Crea una consulta que muestre el nombre comercial, principio activo y laboratorio de los medicamentos fabricados en el laboratorio BAYER.
+SELECT NOMCOMERCIAL,PRINCIPIO, LABORATORIO FROM MEDICAMENTOS
+WHERE LABORATORIO='BAYER';
+-- 16. Crea una consulta que muestre el nombre comercial, principio activo y laboratorio de los
+-- medicamentos fabricados por el laboratorio EUROPHARM y de precio superior a 5 €.
+SELECT NOMCOMERCIAL,PRINCIPIO,LABORATORIO FROM MEDICAMENTOS
+WHERE LABORATORIO='EUROPHARM'
+AND PRECIO>5;
+-- 17. Crea una consulta que muestre el laboratorio y el precio de los medicamentos cuyo principio
+-- activo es PARACETAMOL O VITAMINAS.
+SELECT LABORATORIO, PRECIO FROM MEDICAMENTOS 
+WHERE PRINCIPIO IN('PARACETAMOL','VITAMINAS');
+-- 18. Crea una consulta que muestre el id_ventas de las ventas realizadas en el año 2009.
+SELECT ID_VENTAS FROM VENTAS 
+WHERE year(FECHA_VENTAS) = 2009;
+-- 19. Crea una consulta que muestre el nombre comercial de los medicamentos que empiezan por A,
+-- que valen menos de 3€ y fabricados por el laboratorio AZQUE.
+SELECT NOMCOMERCIAL FROM MEDICAMENTOS
+WHERE NOMCOMERCIAL LIKE 'A%'
+AND PRECIO<3
+AND LABORATORIO ='AZQUE';
+-- 20. Crea una consulta que muestre los empleados con ape1=’perez’. No escribas PEREZ, aunque esté
+-- así en la tabla. Utiliza la función de postgresql LOWER(), que convierte una cadena de caracteres a
+-- minúsculas. (busca más información sobre esta función en Internet). ¿Cuál es la función contraria
+-- que recibe como parámetro una cadena en minúscula y la convierte a mayúsculas?
+SELECT * FROM EMPLEADOS
+WHERE lower(APE1)='perez';
